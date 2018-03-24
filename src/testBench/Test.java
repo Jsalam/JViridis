b22a0ca851287ddcaaa6ae72ad1679fff7955c95
Juan Salamanca <juans123456@gmail.com>
2018-03-23 21:32:40 -0500

    Color gradient added

--- src/testBench/Test.java
+++ src/testBench/Test.java
@@ -7,6 +7,7 @@ public class Test extends PApplet {
 
 	public void settings() {
 		size(400, 200);
+		
 	}
 
 	public void draw() {
@@ -25,6 +26,7 @@ public class Test extends PApplet {
 		// Magma
 		// Another way to set the colorMap
 		ColorMap.getInstance().setColorMap(ColorMap.MAGMA).show(this, 50, 120);
+
 	}
 
 	public static void main(String[] args) {
